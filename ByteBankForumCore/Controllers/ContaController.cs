29b58fc920cb99386c51c9aade1a76fa1480fdff
luisgustavogomes <luisgustavogomes@outlook.com>
2019-04-21 22:12:32 -0300

    Add Identity

---
+++ ByteBankForumCore/Controllers/ContaController.cs
@@ -0,0 +1,24 @@
+﻿using ByteBankForumCore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ByteBankForumCore.Controllers
+{
+    public class ContaController : Controller
+    {
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Registrar(ContaRegistrarViewModel model)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ ByteBankForumCore/Models/ViewModels/ContaRegistrarViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ByteBankForumCore.Models.ViewModels
+{
+    public class ContaRegistrarViewModel
+    {
+
+        [Required(ErrorMessage = "Nome do usuário é obrigatório!")]
+        [MinLength(5, ErrorMessage = "Nome do usuário deve conter no mínimo 5 caracteres")]
+        [MaxLength(50, ErrorMessage = "Nome do usuário deve conter no máximo 50 caracteres")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Nome completo")]
+        [MinLength(5, ErrorMessage = "Nome completo deve conter no mínimo 5 caracteres")]
+        [MaxLength(200, ErrorMessage = "Nome do usuário deve conter no máximo 200 caracteres")]
+        public string NomeCompleto { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "E-mail em formato inválido!")]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Nome completo deve conter no mínimo 5 caracteres")]
+        [MaxLength(200, ErrorMessage = "Nome do usuário deve conter no máximo 200 caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+    }
+}
